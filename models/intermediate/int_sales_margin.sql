fbc8e8074ada635a029178f62c4ec715f8048cf3
christophe.delapaz@gmail.com <christophe.delapaz@gmail.com>
2024-10-29 14:13:22 +0000

    upt int_sales_margin + creation of int_orders_margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,7 +1,5 @@
 select
-    date_date
-    ,orders_id
-    ,s.products_id
+    *
     ,purchase_price * quantity AS purchase_cost
     ,ROUND((revenue - purchase_price* quantity),2) AS margin
 from {{ref("stg_raw__sales")}} AS s
