daf41756420f1a2c39c851d2f96bf113e73f79ce
christophe.delapaz@gmail.com <christophe.delapaz@gmail.com>
2024-10-29 14:05:34 +0000

    updt int_sales_margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,5 +1,9 @@
 select
-    *
-from {{ref("stg_raw__sales")}}
-inner join {{ref("stg_raw__product")}}
+    date_date
+    ,orders_id
+    ,s.products_id
+    ,purchase_price * quantity AS purchase_cost
+    ,ROUND((revenue - purchase_price* quantity),2) AS margin
+from {{ref("stg_raw__sales")}} AS s
+inner join {{ref("stg_raw__product")}} AS p
 USING (products_id)
