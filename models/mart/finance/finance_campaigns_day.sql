08975b58117a442dc4883005fc58f8b6d53412d6
christophe.delapaz@gmail.com <christophe.delapaz@gmail.com>
2024-10-30 13:48:37 +0000

    Création de la table finance_campaigns_days et de son test unique / non null

---
+++ models/mart/finance/finance_campaigns_day.sql
@@ -0,0 +1,19 @@
+SELECT 
+    date_date
+    ,operational_margin - ads_cost AS ads_margin
+    ,average_basket
+    ,operational_margin
+    ,ads_cost
+    ,ads_impression
+    ,ads_clicks
+    ,quantity_of_products_sold AS quantity
+    ,tot_revenue AS revenue
+    ,tot_purchase_cost AS purchase_cost
+    -- ,margin
+    ,tot__shipping_fee AS shipping_fee
+    ,tot_log_cost AS logcost
+    -- ,ship_cost
+FROM {{ref("int_campaigns_day")}} AS c
+INNER JOIN {{ref("finance_days")}} AS f
+USING(date_date)
+ORDER BY date_date DESC
